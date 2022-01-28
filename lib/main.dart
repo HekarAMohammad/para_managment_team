c0161034cac13479bcd49730deae11b7921bc282
hekar <hekar1987@gmail.com>
2022-01-28 19:29:46 +0300

    pull

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:para_managment_team/src/Widget/detailedJobWidget.dart';
 
-import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
@@ -20,5 +20,5 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  runApp(const detailedJobWidget());
 }
